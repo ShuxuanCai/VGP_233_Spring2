a491c7bce9a22b6cdd0bfffa58de0d2dad91e80c
ShuxuanCai <caishuxuan00@gmail.com>
2021-05-05 00:54:34 -0700

    Lab 2

---
+++ Lab_2_Grayblock/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(22.0f, 2.0f, 0.0f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
